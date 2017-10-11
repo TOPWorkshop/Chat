fe6bf6cd5ceef0618567fc24c4fd316f554041a6
Zweer <niccolo@olivieriachille.com>
2017-10-11 18:53:35 +0200

    Moved the password logic to the room

--- app/models/room.js
+++ app/models/room.js
@@ -1,3 +1,6 @@
+const config = require('config');
+const crypto = require('crypto');
+
 module.exports = (sequelize, DataTypes) => {
   const Model = sequelize.define('room', {
     name: {
@@ -5,7 +8,34 @@ module.exports = (sequelize, DataTypes) => {
       unique: true,
       allowNull: false,
     },
+
+    password: {
+      type: DataTypes.STRING,
+      get() { return ''; },
+      set(password) {
+        const salt = crypto.randomBytes(config.get('security.saltLength')).toString('hex');
+        const hash = Model.hashPassword(password, salt);
+
+        this.setDataValue('salt', salt);
+        this.setDataValue('password', hash);
+      },
+    },
+
+    salt: {
+      type: DataTypes.STRING,
+      set() {},
+    },
   });
+
+  Model.hashPassword = function hashPassword(password, salt) {
+    return crypto
+      .pbkdf2Sync(password, salt, config.get('security.iterations'), config.get('security.hashDigest'), config.get('security.hashDigest'))
+      .toString('hex');
+  };
+
+  Model.prototype.validatePassword = function validatePassword(password) {
+    return this.password === this.hashPassword(password, this.salt);
+  };
 
   Model.associate = function associate(models) {
     models.room.hasMany(models.message);
