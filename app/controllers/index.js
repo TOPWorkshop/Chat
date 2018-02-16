19ac4310c3396758907dc4b37d4a611ea583ad29
Zweer <niccolo@olivieriachille.com>
2018-02-16 12:04:39 +0100

    Added the async handler

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -6,9 +6,31 @@ module.exports = class AbstractController {
     this.router = express.Router();
 
     this.initRouter();
+
+    this.promisifyMiddlewares();
   }
 
   initRouter() { // eslint-disable-line class-methods-use-this
     throw new Error('"initRouter" method must be implemented');
+  }
+
+  promisifyMiddlewares() {
+    this.router.stack.forEach(({ route }) => {
+      route.stack.forEach((stack) => {
+        if (stack.handle.constructor.name === 'AsyncFunction') {
+          const oldHandle = stack.handle;
+
+          // eslint-disable-next-line no-param-reassign
+          stack.handle = AbstractController.wrapPromise(oldHandle);
+        }
+      });
+    });
+  }
+
+  static wrapPromise(middleware) {
+    return (request, response, next) => {
+      middleware(request, response, next)
+        .catch(next);
+    };
   }
 };
